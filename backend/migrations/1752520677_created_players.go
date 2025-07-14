af1f337fbc8206ef2bf17e9cb0f3678e2543c23d
Nevimmu <nevimmu@gmail.com>
2025-07-14 21:24:45 +0200

    feat(backend): setup the backend collections

---
+++ backend/migrations/1752520677_created_players.go
@@ -0,0 +1,135 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"cascadeDelete": false,
+					"collectionId": "pbc_879072730",
+					"hidden": false,
+					"id": "relation590033292",
+					"maxSelect": 1,
+					"minSelect": 0,
+					"name": "game",
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "relation"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1579384326",
+					"max": 0,
+					"min": 0,
+					"name": "name",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": true,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"exceptDomains": [],
+					"hidden": false,
+					"id": "url4101391790",
+					"name": "url",
+					"onlyDomains": [],
+					"presentable": false,
+					"required": true,
+					"system": false,
+					"type": "url"
+				},
+				{
+					"hidden": false,
+					"id": "json2678318100",
+					"maxSize": 0,
+					"name": "results",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "json"
+				},
+				{
+					"hidden": false,
+					"id": "number848901969",
+					"max": null,
+					"min": null,
+					"name": "score",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_3072146508",
+			"indexes": [],
+			"listRule": null,
+			"name": "players",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": null
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3072146508")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	})
+}
