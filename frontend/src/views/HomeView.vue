9f5a5ba56282fcb2de0518ad5e29805da7abe2e5
Nevimmu <nevimmu@gmail.com>
2025-07-14 23:50:01 +0200

    feat(frontend): added generateRandomCode

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,9 +7,20 @@ import JoinGame from '@/components/JoinGame.vue'
 
 const router = useRouter()
 
-function createGame() {
+const generateRandomCode = (length: number) => {
+	let result = ''
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+	const charactersLength = characters.length
+
+	for (let i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength))
+	}
+
+	return result
+}
+
+const createGame = () => {
 	// TODO: game creation
-	console.log('Create game')
 }
 </script>
 
