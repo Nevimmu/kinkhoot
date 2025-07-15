7b8779eaac26d7061be920d3787710de27fecdb3
Nevimmu <nevimmu@gmail.com>
2025-07-15 02:43:03 +0200

    feat(frontend): created the host view template

--- frontend/src/views/HostView.vue
+++ frontend/src/views/HostView.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { useGameStore } from '@/stores/game';
+
+const gameStore = useGameStore()
+</script>
 
 <template>
-	<p>Host</p>
+	<div class="flex flex-1 flex-col p-5 w-full items-center">
+		<span class="text-center text-xl">Game code:</span>
+		<span class="text-center font-bold text-7xl">{{ gameStore.gameId }}</span>
+		<Button class="w-xs">Start the game</Button>
+		<div class="grid grid-cols-1 w-lg bg-amber-200">
+		</div>
+	</div>
 </template>
