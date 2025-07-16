232ed90e6dce700627a614fa9d65340a0522b9be
Nevimmu <nevimmu@gmail.com>
2025-07-16 22:31:59 +0200

    refactor: removed authStore to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -13,7 +12,4 @@ const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 
-const authStore = useAuthStore(pinia)
-authStore.init().finally(() => {
-	app.mount('#app')
-})
+app.mount('#app')
