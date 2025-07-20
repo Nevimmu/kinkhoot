66f6963a2559d65fa60b81e10f0eb4d1ca59bffb
Nevimmu <nevimmu@gmail.com>
2025-07-20 07:10:42 +0200

    feat(frontend): now with confetti!

--- frontend/src/components/host/Finished.vue
+++ frontend/src/components/host/Finished.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { usePlayerStore } from '@/stores/player';
+import { vConfetti } from '@neoconfetti/vue'
 
 const playerStore = usePlayerStore()
+const height = window.innerHeight
 </script>
 
 <template>
+	<div v-confetti="{stageHeight: height, particleCount: 300}"/>
 	<div class="text-center">
 		<h2 class="text-2xl font-bold">Game Finished!</h2>
 		<div class="flex flex-col">
