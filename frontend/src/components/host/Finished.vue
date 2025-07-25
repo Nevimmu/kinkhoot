8a0cab6f87dfd055a4a37096802c38e2801828fe
Nevimmu <nevimmu@gmail.com>
2025-07-26 00:26:32 +0200

    fix(results): ensure players are displayed in the correct order on the final page

--- frontend/src/components/host/Finished.vue
+++ frontend/src/components/host/Finished.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { usePlayerStore } from '@/stores/player';
 import { vConfetti } from '@neoconfetti/vue'
+import { computed } from 'vue';
 
 const playerStore = usePlayerStore()
 const height = window.innerHeight
+
+const sortedPlayers = computed(() => {
+  return [...playerStore.players].sort((a, b) => b.score - a.score)
+})
 </script>
 
 <template>
@@ -11,7 +16,7 @@ const height = window.innerHeight
 	<div class="text-center">
 		<h2 class="text-2xl font-bold">Game Finished!</h2>
 		<div class="flex flex-col">
-			<span v-for="player in playerStore.players" :key="player.score" class="text-2xl">
+			<span v-for="player in sortedPlayers" :key="player.name" class="text-2xl">
 				{{ player.name }} - {{ player.score }}
 			</span>
 		</div>
